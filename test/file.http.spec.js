1377eaa217a0c969d61cf579cad2c22e5da19dc6
lykmapipo <lallyelias87@gmail.com>
2019-06-02 20:46:47 +0300

    test: ensure properties assertions on file http specs

--- test/file.http.spec.js
+++ test/file.http.spec.js
@@ -5,25 +5,41 @@ import {
 import {
   clear as clearHttp,
   testRouter,
-  testUpload,
 } from '@lykmapipo/express-test-helpers';
 import fileRouter from '../src/file.http.router';
+
+const properties = [
+  'length',
+  'chunkSize',
+  'uploadDate',
+  'md5',
+  'filename',
+  'contentType',
+  'aliases',
+  'metadata',
+  '_id',
+  '__v',
+];
+
+const options = {
+  pathSingle: '/files/:bucket/:id',
+  pathList: '/files/:bucket',
+  pathSchema: '/files/:bucket/schema',
+};
 
 describe.only('HTTP API', () => {
   before(() => clearHttp());
   before(done => clearDatabase(done));
 
-  const options = {
-    pathSingle: '/files/:bucket/:id',
-    pathList: '/files/:bucket',
-    pathSchema: '/files/:bucket/schema',
-  };
-
-  expect(testUpload).to.exist;
-
   it('should handle HTTP GET on /files/:bucket/schema', done => {
     const { testGetSchema } = testRouter(options, fileRouter);
-    testGetSchema({ bucket: 'files' }).expect(200, done);
+    testGetSchema({ bucket: 'files' }).expect(200, (error, { body }) => {
+      expect(error).to.not.exist;
+      expect(body).to.exist;
+      expect(body).to.have.all.keys('title', 'type', 'properties');
+      expect(body.properties).to.have.all.keys(...properties);
+      done(error, body);
+    });
   });
 
   after(() => clearHttp());
